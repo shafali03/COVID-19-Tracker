92880b26d175c73211bbb3bbcbb17683ae9e6ccb
shafali03 <shaf03sm@gmail.com>
2020-11-13 21:12:10 +0000

    Displaying coronavirus cases of each country using the popup

--- src/util.js
+++ src/util.js
@@ -41,7 +41,22 @@ export const showDataOnMap = (data, casesType = "cases") =>
       }
     >
       <Popup>
-        <h2>Popup</h2>
+        <div className='info-container'>
+          <div
+            className='info-flag'
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          />
+          <div className='info-name'>{country.country}</div>
+
+          <div className='info-confirm'>
+            Cases: {numeral(country.cases).format("0.0")}</div>
+
+          <div className='info-recovered'>
+            Recovered: {numeral(country.recovered).format("0,0")}</div>
+
+          <div className='info-deaths'>
+            Deaths: {numeral(country.deaths).format("0.0")}</div>
+        </div>
       </Popup>
     </Circle>
   ))
